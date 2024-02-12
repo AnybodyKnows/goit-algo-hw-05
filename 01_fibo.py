900837ef2100bcab6003f669a397d8a6660ce881
AnybodyKnows <evgeniiolavrik@gmail.com>
2024-02-12 20:32:59 +0100

    01 fixed

--- 01_fibo.py
+++ 01_fibo.py
@@ -1,38 +1,25 @@
 import time as t
 
-def caching_fibonacci(n):
+def caching_fibonacci():
     """Function with cashe"""
     cashe = {}
 
-    def fibo(n):
+    def fibonacci(n):
         if n <=0: return 0
         elif n==1: return 1
         elif n in cashe:
             # print(f"easy work--{n}")
             return cashe[n]
-        z = fibo(n-1) + fibo(n-2)
+        z = fibonacci(n-1) + fibonacci(n-2)
         cashe[n]=z
         # print(f"hard work--{n}")
         return z
-    return fibo(n)
-
-def fibonacci(n):
-    """Function WITHOUT cashe"""
-    def fibo(n):
-        if n <=0: return 0
-        elif n==1: return 1
-        z = fibo(n-1) + fibo(n-2)
-        return z
-    return fibo(n)
+    return fibonacci
 
 
-itter = 35
-nach = t.time()
-a = caching_fibonacci(itter)
-print(a)
-print(f"time of running {itter} times with cashe {float(t.time()-nach)}")
 
-nach1 = t.time()
-a = fibonacci(itter)
-print(a)
-print(f"time of running {itter} times WITHOUT cashe  {float(t.time()-nach1)}")
+
+
+fibo = caching_fibonacci()
+print(fibo(10))
+print(fibo(15))
