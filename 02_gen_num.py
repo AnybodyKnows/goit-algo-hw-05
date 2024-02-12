2ee879e10ec98270923100bc1ca4df3552e0f0ff
AnybodyKnows <evgeniiolavrik@gmail.com>
2024-02-12 23:34:16 +0100

    02 fixed

--- 02_gen_num.py
+++ 02_gen_num.py
@@ -3,11 +3,11 @@ from typing import Callable
 from functools import reduce
 
 text = """Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, 
-доповнений додатковими надходженнями 27.45 і 324.00 доларів."""
+доповнений додатковими надходженнями 27.45 і 324.00 доларів 5.5."""
 
 
 def generator_numbers(txt: str):
-    nums = map(float, filter(lambda x: re.match(r"[0-9]",x), txt.split()))
+    nums = map(float, list(re.sub(r"\s", "", a) for a in re.findall(r"\s\d+\.\d+\s|\s\d+\s", text)))
     for num in nums:
         yield num
 
@@ -19,3 +19,6 @@ def sum_profit(text, gen: Callable):
 total_income = sum_profit(text, generator_numbers)
 print(f"Total income: {total_income}")
 
+
+
+# print (list(map(float, list(re.sub(r"\s", "", a) for a in re.findall(r"\s\d+\.\d+\s|\s\d+\s", text)))))
